949aba48b981b79e3ab9f231a77c46e7daa45ea1
文昊 胡 <hwh19981124@hotmail.com>
2019-11-05 10:08:29 +0800

    unsolve pipe

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+#include <Windows.h>
+#include <tchar.h>
+constexpr int BUFSIZE = 512;
 int main() {
+
+	HANDLE hpipe = INVALID_HANDLE_VALUE, hThread = NULL;
+	LPCTSTR lpszPipename = TEXT("\\\\.\\pipe\\mynamedpipe");
+	hpipe = CreateNamedPipe(
+		lpszPipename,
+		PIPE_ACCESS_DUPLEX,
+		PIPE_TYPE_MESSAGE |
+		PIPE_READMODE_MESSAGE |
+		PIPE_WAIT,
+		PIPE_UNLIMITED_INSTANCES,
+		BUFSIZE,
+		BUFSIZE,
+		0,
+		NULL
+	);
+	if (hpipe == INVALID_HANDLE_VALUE)
+	{
+
+		_tprintf(TEXT("CreateNamedPipe failed, GLE=%d.\n"), GetLastError());
+		return -1;
+	}
+
+	//bool fConnected = ConnectNamedPipe(hpipe, NULL) ?
+	//	TRUE : (GetLastError() == ERROR_PIPE_CONNECTED);
+
+	CloseHandle(hpipe);
+
 	std::vector<int> page_num;
 	std::fstream infile("infile.in", std::ios::in);
 	while (!infile.eof())
@@ -10,7 +39,7 @@ int main() {
 		int num_temp;
 		infile >> num_temp;
 		page_num.push_back(num_temp);
-		
+
 	}
 	infile.close();
 	system("pause");
