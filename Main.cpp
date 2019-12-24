cb83574dd73bc022e86f9d6ad7d8ed477e73ffb4
文昊 胡 <hwh19981124@hotmail.com>
2019-12-24 08:58:04 +0800

    change

--- Main.cpp
+++ Main.cpp
@@ -14,68 +14,6 @@ int main() {
 
 	//HANDLE hpipe = INVALID_HANDLE_VALUE;
 
-	//hpipe = CreateNamedPipe(
-	//	lpszPipename,
-	//	PIPE_ACCESS_DUPLEX,
-	//	PIPE_TYPE_MESSAGE |
-	//	PIPE_READMODE_MESSAGE |
-	//	PIPE_WAIT,
-	//	PIPE_UNLIMITED_INSTANCES,
-	//	BUFSIZE,
-	//	BUFSIZE,
-	//	0,
-	//	NULL
-	//);
-
-	//if (hpipe == INVALID_HANDLE_VALUE)
-	//{
-
-	//	_tprintf(TEXT("CreateNamedPipe failed, GLE=%d.\n"), GetLastError());
-	//	return -1;
-	//}
-
-	//DWORD dwWritten;
-	//HANDLE hpipe_write;
-	//hpipe_write = CreateFile(
-	//	lpszPipename,
-	//	GENERIC_READ | GENERIC_WRITE,
-	//	0,
-	//	NULL,
-	//	OPEN_EXISTING,
-	//	0,
-	//	NULL
-	//);
-
-	//if (hpipe_write != INVALID_HANDLE_VALUE)
-	//{
-	//	WriteFile(hpipe_write,
-	//		"test message\n",
-	//		15,
-	//		&dwWritten,
-	//		NULL
-	//	);
-	//	CloseHandle(hpipe_write);
-	//}
-
-
-	//BOOL fConnected = FALSE;
-	//if (hpipe != INVALID_HANDLE_VALUE)
-	//{
-	//	fConnected = ConnectNamedPipe(hpipe, NULL) ?
-	//		TRUE : (GetLastError() == ERROR_PIPE_CONNECTED);
-	//	if (fConnected)
-	//	{
-	//		while (ReadFile(hpipe, buffer, sizeof(buffer) - 1, &dwRead, NULL) != FALSE)
-	//		{
-	//			/* add terminating zero */
-	//			buffer[dwRead] = '\0';
-
-	//			/* do something with data in buffer */
-	//			printf("%s", buffer);
-	//		}
-	//	}
-	//	DisconnectNamedPipe(hpipe);
-	//}
 
 
 	//读入进程访问序列
